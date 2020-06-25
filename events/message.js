7ec8d09f8c7048f3366b704674c4f94df8f612cb
Gdeft <imanelientarkan18@gmail.com>
2020-06-25 16:50:02 +0700

    Commands aren't available on DM now

--- events/message.js
+++ events/message.js
@@ -1,13 +1,28 @@
 module.exports = (client, message) => {
-  if (message.author.bot) return;
-  if (message.content.indexOf(client.config.prefix) !== 0) return;
+  if(message.author.bot) return;
+  if(message.content.indexOf(client.config.prefix) !== 0) return;
 
   const args = message.content.slice(client.config.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
 
   const cmd = client.commands.get(command);
 
-  if (!cmd) return;
+  if(!cmd) return;
+
+  if(cmd && !message.guild && message.author.id !== client.config.ownerID){
+    message.channel.send({embed: {
+        color: '#ff0000',
+        author: {
+          name: client.user.username,
+          icon_url: client.user.avatarURL()
+        },
+        fields: [{
+          name: 'Error',
+          value: 'Cannot execute that command inside DMs!'
+        }]
+      }
+    })
+  }
 
   cmd.run(client, message, args);
 };
