177523c4e1b9037c5a970027126b73ac297f5830
Gdeft <imanelientarkan18@gmail.com>
2020-06-25 16:36:26 +0700

    Google color changes

--- commands/google.js
+++ commands/google.js
@@ -2,7 +2,7 @@ exports.run = (client, message, args) => {
   const google = args.join(' ');
   const search = google.replace(/ /g, '%20');
   const embed = { embed: {
-      color: 3400473,
+      color: '#4885ed',
       author: {
         name: client.user.username,
         icon_url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Google_Chrome_icon_%28September_2014%29.svg/1200px-Google_Chrome_icon_%28September_2014%29.svg.png'
