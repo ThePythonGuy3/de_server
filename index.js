8d7ee1049075d5847a18205a6b68ebd14ea9c410
Gdeft <imanelientarkan18@gmail.com>
2020-06-25 09:52:54 +0700

    h

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const fs = require("fs");
 
 const client = new Discord.Client();
 const config = require("./config.json");
-// We also need to make sure we're attaching the config to the CLIENT so it's accessible everywhere!
 client.config = config;
 
 client.on('message', message => {
