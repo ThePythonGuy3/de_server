87f06d6bd948948e15c244cda61fee17ab6cf109
ThePythonGuy <45978337+ThePythonGuy3@users.noreply.github.com>
2020-06-27 10:38:36 +0200

    Create suggestfix.js
    Maybe this fixes the lost arg

---
+++ commands/suggestfix.js
@@ -0,0 +1,60 @@
+exports.run = (client, message) => {
+  var args1 = message.content.slice(client.config.prefix.length).trim().split(/;/g);
+  var command1 = args1.shift().toLowerCase();
+  const embed = { embed: {
+      color: '#00ff00',
+      author: {
+        name: message.author.username,
+        icon_url: message.author.avatarURL()
+      },
+      fields: [{
+        name: `Suggestion`,
+        value: `${args[0]}\n\n${args[1]}\n\n${args[2]}`
+      }],
+      footer: {
+        text: 'This suggestion might be reviewed in the following hours, days, weeks, or never.'
+      }
+    }
+  }
+  let suggestChannel = message.channel.id == client.config.suggestionsID;
+
+  if (args1.length == 2) return client.channels.cache.get(client.config.suggestionsID).send(embed);
+    else if (!args1){
+      message.channel.send({embed: {
+          color: '#ff0000',
+          author: {
+            name: client.user.username,
+            icon_url: client.user.avatarURL()
+          },
+          fields: [{
+            name: 'Error',
+            value: 'You need to include the arguments!'
+          }]
+        }
+      })
+    } else {
+        message.channel.send({embed: {
+          color: '#ff0000',
+          author: {
+            name: client.user.username,
+            icon_url: client.user.avatarURL()
+          },
+          fields: [{
+            name: 'Error',
+            value: 'You need 3 arguments to type in, type **.help suggest** for help.'
+          }]
+        }
+      })
+    }
+}
+
+exports.conf = {
+  enabled: true,
+  guildOnly: true
+}
+
+exports.h = {
+  name: 'Suggest',
+  desc: 'Redirects your suggestion to the <#704356079756247160> channel.',
+  usage: '**.suggest** *<h>;<mod-role>;<suggestion>*'
+}
